32c8cf876ffe3a48320ce66b9018a37f6a5dc1ac
rohan <halo.zero00@gmail.com>
2015-07-14 17:46:24 -0700

    Adding score keeper to functions

---
+++ Week_05_Intro_JavaScript/08_functions/starter_code/score_keeper/main.js
@@ -0,0 +1,26 @@
+var score = 0;
+
+function zero() {
+  score = 0;
+  document.getElementById('result').innerHTML = score;
+}
+function addFive() {
+  score = score + 5;
+  document.getElementById('result').innerHTML = score;
+}
+
+function addTen() {
+  score = score + 10;
+  document.getElementById('result').innerHTML = score;
+}
+
+function minusOne() {
+  score = score - 1;
+  document.getElementById('result').innerHTML = score;
+}
+
+document.getElementById('zero').onclick = zero;
+document.getElementById('add5').onclick = addFive;
+document.getElementById('add10').onclick = addTen;
+document.getElementById('sub1').onclick = minusOne;
+
