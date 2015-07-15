c19d0b63a82b6b2f3352afa3009efe47c8747470
Rohan Dhaimade <rohan@ginger.io>
2015-07-15 13:06:20 -0700

    modifying theme switcher to be right function order

--- Week_05_Intro_JavaScript/08_functions/starter_code/theme_switcher/main.js
+++ Week_05_Intro_JavaScript/08_functions/starter_code/theme_switcher/main.js
@@ -1,28 +1,26 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+// this is a comment, nothing in this line is evaluated by javascript
+// use it explain what's going on
+function switchGray() {
+  document.getElementsByTagName('body')[0].className = 'gray-background';
+}
 
-  document.getElementById('grayButton').onclick = switchGray;
-  document.getElementById('whiteButton').onclick = switchWhite;
-  document.getElementById('blueButton').onclick = switchBlue;
-  document.getElementById('yellowButton').onclick = switchYellow;
+// code inside functions are not evaluated until they are called
+// when you tell an element 'onClick' = a function, it will call that function
+// and evaluate any code inside of it.
+function switchWhite() {
+  document.getElementsByTagName('body')[0].className = 'white-background';
+}
 
-  // this is a comment, nothing in this line is evaluated by javascript
-  // use it explain what's going on
-  function switchGray() {
-    document.getElementsByTagName('body')[0].className = 'gray-background';
-  }
+function switchBlue() {
+  document.getElementsByTagName('body')[0].className = 'blue-background';
+}
 
-  // code inside functions are not evaluated until they are called
-  // when you tell an element 'onClick' = a function, it will call that function
-  // and evaluate any code inside of it.
-  function switchWhite() {
-    document.getElementsByTagName('body')[0].className = 'white-background';
-  }
+function switchYellow() {
+  document.getElementsByTagName('body')[0].className = 'yellow-background';
+}
 
-  function switchBlue() {
-    document.getElementsByTagName('body')[0].className = 'blue-background';
-  }
+document.getElementById('grayButton').onclick = switchGray;
+document.getElementById('whiteButton').onclick = switchWhite;
+document.getElementById('blueButton').onclick = switchBlue;
+document.getElementById('yellowButton').onclick = switchYellow;
 
-  function switchYellow() {
-    document.getElementsByTagName('body')[0].className = 'yellow-background';
-  }
-});
