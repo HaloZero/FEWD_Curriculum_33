368c97b66840d9546a7182cffb4625f4a7783433
Rohan Dhaimade <rohan@ginger.io>
2015-07-15 16:56:46 -0700

    Changing instructions, adding solutions

--- Week_05_Intro_JavaScript/08_functions/starter_code/bank_balance/main.js
+++ Week_05_Intro_JavaScript/08_functions/starter_code/bank_balance/main.js
@@ -15,15 +15,21 @@ checkBalance();
 // Make sure the balance has changed. Try buying multiple coffees.
 
 // Step 2: Create a new function that lets you buy laundry detergent for 10
+// Define this function above the comment Step 1.
+// This allows us to keep all our functions together.
 // Make sure it works by buying detergent and then checking the balance
 
 // Step 3: Create a new function that lets you buy a bed frame for 125
+// Define this function above the comment Step 1.
+// This allows us to keep all our functions together.
 // Make sure it works by buying a bed frame and then checking the balance
 
 // Step 4: Reduce the starting balance to 100
+// Modify the value of balance on the first line of our JS to be 100
 
-// Step 5: Let's make sure that you can never buy something if you can't afford it
-// If somebody tries to buy an object that they can't afford.
-// Print out "You can't buy that!";
+
+// Step 5: Let's make sure that you can never buy something if you can't afford it.
+// Modify all your functions so that if somebody tries to buy an
+// object that they can't afford. You print to the console "You can't buy that!";
 
 // Step 6: Utilize our knowledge of functions to reduce repeatable code.
