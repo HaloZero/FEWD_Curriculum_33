9d916cb962980dc87ef43fdf1f9173bb860fc6cb
Rohan Dhaimade <rohan@ginger.io>
2015-07-15 18:23:35 -0700

    Adding calculator

---
+++ Week_05_Intro_JavaScript/08_functions/starter_code/calculator/exercise.js
@@ -0,0 +1,88 @@
+function exercise1() {
+  var a = add(1, 2);
+  var b = add(10, 6);
+  var c = add(a,b);
+  checkValue(19, c, "exercise-1");
+}
+
+function exercise2() {
+  var a = sub(10, 2);
+  var b = add(25, 5);
+  var c = add(b,a);
+  checkValue(12, c, "exercise-2");
+}
+
+function exercise3() {
+  var a = add(5, 1);
+  var b = sub(2, 3);
+  var c = sub(a, b);
+  checkValue(7, c, "exercise-3");
+}
+
+function exercise4() {
+  var a = max(5, -1);
+  var b = max(10, 3);
+  var c = max(a, b);
+  checkValue(10, c, "exercise-4");
+}
+
+function exercise5() {
+  var a = min(5, -1);
+  var b = min(10, 3);
+  var c = min(a, b);
+  checkValue(-1, c, "exercise-5");
+}
+
+function exercise6() {
+  var a = minOf3(5, -1, 10);
+  var b = minOf3(10, 3, 1);
+  var c = minOf3(a, b, -5);
+  checkValue(-5, c, "exercise-6");
+}
+
+function exercise7() {
+  var a = maxOf3(5, -1, 10);
+  var b = maxOf3(111, 3, 1);
+  var c = maxOf3(a, b, -5);
+  checkValue(111, c, "exercise-7");
+}
+
+function exercise8() {
+  var a = maxOf3(5, -1, 10);
+  var b = minOf3(13, 3, 1);
+  var c = max(a, b);
+  checkValue(13, c, "exercise-8");
+}
+
+function exercise9() {
+  var a = maxOfAll(5, 10, 111, 55);
+  var b = maxOfAll(1, 1, 1, 1, 1, 1, 5, 10);
+  var c = maxOfAll(a, b, 501, 1);
+  checkValue(501, c, "exercise-9");
+}
+
+function exercise10() {
+  var a = minOfAll(5, 10, 111, 55);
+  var b = minOfAll(1, 1, 1, 1, 1, 1, 5, 10);
+  var c = minOfAll(a, b, 501, -1);
+  checkValue(-1, c, "exercise-10");
+}
+
+function checkValue(value, correctValue, elementId) {
+  if (value == correctValue) {
+    document.getElementById(elementId).className = "success";
+  } else {
+    document.getElementById(elementId).className = "error";
+  }
+}
+
+document.getElementById("exercise-1").onclick = exercise1;
+document.getElementById("exercise-2").onclick = exercise2;
+document.getElementById("exercise-3").onclick = exercise3;
+document.getElementById("exercise-4").onclick = exercise4;
+document.getElementById("exercise-5").onclick = exercise5;
+document.getElementById("exercise-6").onclick = exercise6;
+document.getElementById("exercise-7").onclick = exercise7;
+document.getElementById("exercise-8").onclick = exercise8;
+document.getElementById("exercise-9").onclick = exercise9;
+document.getElementById("exercise-10").onclick = exercise10;
---
+++ Week_05_Intro_JavaScript/08_functions/starter_code/calculator/main.js
@@ -0,0 +1,34 @@
+// Step 1: Create a function add that takes in two arguments and returns
+// their sum. You can assume they're always numbers
+
+// Step 2: Create a function sub that takes in two arguments and returns
+// their difference. You can assume they're always numbers
+
+// Step 3: Create a function max that takes in two arguments and returns
+// the largest number. You can assume they're always numbers
+
+// Step 4: Create a function min that takes in two arguments and returns
+// the smallest number. You can assume they're always numbers
+
+// Step 5: Create a function maxOf3 that takes in three arguments and returns
+// the largest number. You can assume they're always numbers. You should
+// utilize the max function you already created!
+
+// Step 6: Create a function minOf3 that takes in three arguments and returns
+// the smallest number. You can assume they're always numbers. You should
+// utilize the min function you already created!
+
+// Bonus
+// Step 7: Create a function maxOfAll that takes in any number of arguments
+// and returns the largest number. You can assume they're always numbers.
+// You should utilize the max function you already created!
+// This means you can pass in 2 or more arguments!
+// You're hint for googling is going to be "javascript unlimited arguments"
+// You'll also need to learn how to loop over an array
+
+// Step 8: Create a function minOfAll that takes in any number of arguments
+// and returns the largest number. You can assume they're always numbers.
+// This means you can pass in 2 or more arguments!
+// You should utilize the min function you already created!
+// You're hint for googling is going to be "javascript unlimited arguments"
+// You'll also need to learn how to loop over an array
