dec0f0b3c2d7b26be4cf5fac6e6a5c7c3668c0a3
rohan <halo.zero00@gmail.com>
2015-07-15 23:39:50 -0700

    Matching calculator assignment to steps, adding solution

---
+++ Week_05_Intro_JavaScript/08_functions/solution/calculator/main.js
@@ -0,0 +1,77 @@
+// Step 1: Create a function add that takes in two arguments and returns
+// their sum. You can assume they're always numbers
+
+function add(a, b) {
+	return a + b;
+}
+// Step 2: Create a function sub that takes in two arguments and returns
+// their difference. You can assume they're always numbers
+
+function sub(a, b) {
+	return a - b;
+}
+// Step 3: Create a function max that takes in two arguments and returns
+// the largest number. You can assume they're always numbers
+
+function max(a, b) {
+	if (a > b) {
+		return a; 
+	} else {
+		return b;
+	}
+}
+// Step 4: Create a function min that takes in two arguments and returns
+// the smallest number. You can assume they're always numbers
+
+function min(a, b) {
+	if (a < b) {
+		return a; 
+	} else {
+		return b;
+	}
+}
+// Step 5: Create a function maxOf3 that takes in three arguments and returns
+// the largest number. You can assume they're always numbers. You should
+// utilize the max function you already created!
+
+function maxOf3(a, b, c) {
+	return max(max(a, b), c);
+}
+// Step 6: Create a function minOf3 that takes in three arguments and returns
+// the smallest number. You can assume they're always numbers. You should
+// utilize the min function you already created!
+
+function minOf3(a, b, c) {
+	return min(min(a, b), c);
+}
+
+// Bonus
+// Step 7: Create a function maxOfAll that takes in any number of arguments
+// and returns the largest number. You can assume they're always numbers.
+// You should utilize the max function you already created!
+// This means you can pass in 2 or more arguments!
+// You're hint for googling is going to be "javascript unlimited arguments"
+// You'll also need to learn how to loop over an array
+
+function maxOfAll() {
+	var maxTotal = 0;
+	for (var i = 0; i < arguments.length; i++) {
+		maxTotal = max(maxTotal, arguments[i]);
+	}
+	return maxTotal;
+}
+// Step 8: Create a function minOfAll that takes in any number of arguments
+// and returns the largest number. You can assume they're always numbers.
+// This means you can pass in 2 or more arguments!
+// You should utilize the min function you already created!
+// You're hint for googling is going to be "javascript unlimited arguments"
+// You'll also need to learn how to loop over an array
+// Useful: The largest number can be is Number.MAX_VALUE;
+
+function minOfAll() {
+	var minTotal = Number.MAX_VALUE;
+	for (var i = 0; i < arguments.length; i++) {
+		minTotal = min(minTotal, arguments[i]);
+	}
+	return minTotal;
+}
