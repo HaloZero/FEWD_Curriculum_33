a268476b47d3b5d43ccdfbab2de9719ae350d15d
Rohan Dhaimade <rohan@ginger.io>
2015-07-20 13:08:15 -0700

    Removing guide

--- Week_05_Intro_JavaScript/08_functions/starter_code/high_low_dice/main.js
+++ Week_05_Intro_JavaScript/08_functions/starter_code/high_low_dice/main.js
@@ -5,13 +5,3 @@
 
 // PROTIP: You can't start a function or variable name with a number.
 // So don't name your function 7.
-
-// Step 1: Setup on our on click events
-// Step 2: Test them, make sure your onclick events work
-// Step 3: Pick a function to implement
-// Step 4: Psuedocode that function
-// Step 5: Implement a line from the psuedocode,
-// but don't worry about updating the HTML
-// Step 6: Make sure your functions works only in JS. Use console.log!
-// Step 7: Update the HTML appropriately in the spots that you need.
-// Step 8: Repeat step 3-7 for each function!
