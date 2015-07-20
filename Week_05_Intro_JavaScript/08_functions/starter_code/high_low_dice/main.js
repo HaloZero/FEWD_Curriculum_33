d4b04e78fb7d80ee72d484ce602e2c9ff398ab3d
Rohan Dhaimade <rohan@ginger.io>
2015-07-20 12:50:08 -0700

    High low dice

---
+++ Week_05_Intro_JavaScript/08_functions/starter_code/high_low_dice/main.js
@@ -0,0 +1,14 @@
+// You have a functions available to you.
+
+// Function 1 available to you:
+// rollADice, which returns a number from 1 to 6 randomly
+
+// Step 1: Setup on our on click events
+// Step 2: Test them, make sure your onclick events work
+// Step 3: Pick a function to implement
+// Step 4: Psuedocode that function
+// Step 5: Implement a line from the psuedocode,
+// but don't worry about updating the HTML
+// Step 6: Make sure your functions works only in JS. Use console.log!
+// Step 7: Update the HTML appropriately in the spots that you need.
+// Step 8: Repeat step 3-7 for each function!
