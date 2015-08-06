224d554d7795324eb37ab6bad55b186e1f9fec40
rohan <rohan.dhaimade@gmail.com>
2015-08-05 23:18:28 -0700

    Updating text and cleaning up code references

--- Week_08_AJAX/17_ajax_lab/starter_code/itunes_artists_real/js/main.js
+++ Week_08_AJAX/17_ajax_lab/starter_code/itunes_artists_real/js/main.js
@@ -1,24 +1,33 @@
 // Step 1)
-// Take your previous lab solution and copy and paste it here (the Javascript only)
-// Get that working. I've removed the example song so make sure your code hasn't broken because of it.
+
+// Take your previous lab solution and copy and paste it here
+// All the Javascript! Clean it up so you should be only only looking at the proper code.
+// Make sure that works.
 // If you didn't finish the lab, finish it up for Step 1
 
 // Step 2)
-// Delete the variable musicPlaylist
+
+// Delete the giant JSON.
 // now instead make an AJAX request to this url: "https://itunes.apple.com/search?term=abba"
-// use the response JSON from that request to populate the page.
+// use the response JSON from that request to populate the page
 
 // Step 3)
 // Now the hard part. Instead of doing a request to https://itunes.apple.com/search?term=abba
-// I want you to generate the URL for the AJAX request based on the text
-// inside the input element on the page.
-// So if I type Journey into that input and hit submit.
-// it should create the url
+// Generate the url from the value of the input box in the search box.
+// I type Journey into that input and hit submit it should create the url
+
 // https://itunes.apple.com/search?term=journey
+
 // and do an AJAX request to that URL
+// The entries should now be journey songs.
 
 // Step 4)
 // We get back more songs from the API than we have!
 // Use our knowledge of creating HTML in Javascript to
-// show all the songs properly. The first 3 songs you can skip
-// so start your loop at index = 2 instead of 0.
+// show all the songs properly. 
+
+// Remember the basic advice is:
+// Create your HTML nodes
+// Append them to the appropriate spots
+// Then use the original loop to modify the elements.
+// 
