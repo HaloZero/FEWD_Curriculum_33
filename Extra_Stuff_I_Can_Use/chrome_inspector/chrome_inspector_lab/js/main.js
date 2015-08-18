cdf53df0f2c7c3d00e3c9a6181637c8698825db1
Rohan Dhaimade <rohan@ginger.io>
2015-08-17 17:08:00 -0700

    Adding chrome inspector notes and labs
    Moving yelp server and yelp api into same folder, adding code along for chrome inspector
    
    Removing yelpserver since it's own git repo

---
+++ Extra_Stuff_I_Can_Use/chrome_inspector/chrome_inspector_lab/js/main.js
@@ -0,0 +1,70 @@
+var humanScore = 0;
+var botScore = 0;
+
+function chooseRock() {
+  var humanChoice = "rock";
+  var botChoice = botChooseType);
+  if (botChoice === "scissors") {
+    humanWon(humanChoice, botChoice);
+  } else if (botChoice === "paper") {
+    botWon(humanChoice, botChoice);
+  } else {
+    tieGame(humanChoice, botChoice);
+  }
+}
+
+function chooseScissors() {
+  var humanChoice = "scissors";
+  var botChoice = botChooseType();
+  if (botChoice = "paper") {
+    humanWon(humanChoice, botChoice);
+  } else if (botChoice === "rock") {
+    botWon(humanChoice, botChoice);
+  } else {
+    tieGame(humanChoice, botChoice);
+  }
+}
+
+function choosePaper() {
+  var humanChoice = "paper";
+  var botChoice = botChooseType();
+  if (botChoice == "scissors") {
+    humanWon(humanChoice, botChoie);
+  } else if (botChoice == "rock") {
+    humanWon(humanChoice, botChoice);
+  } else {
+    tieGame(humanChoice, botChoice);
+  }
+}
+
+function humanWon(humanChoice, botChoice) {
+  var statusText = baseStatusText(humanChoice, botChoice) + " You won!";
+  humanScore += 1;
+  $("#status").text(statusText);
+  $("#humanScore").text(humanScore);
+}
+
+function botWon(humanChoice, botChoice) {
+  var statusText = baseStatusText(humanChoice, botChoice) + " You lost!";
+  botScore += 1;
+  $("#status").text(statusText);
+  $("#botScore").text(botScore);
+}
+
+function tieGame(humanChoice, botChoice) {
+  var statusText = baseStatusText(humanChoice, botChoice) + " You tied!";
+  $("#status").text(statusText);
+}
+
+function baseStatusText(humanChoice, botChoice) {
+  return "The bot played " + botChoice + " and you played " + humanChoice + ".";
+}
+
+function botChooseType() {
+  var choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random()*3)];
+}
+
+$("#rock").click(chooseRock);
+$("#scissors").click(chooseScissors);
+$("#paper").click(choosePaper);
---
+++ Extra_Stuff_I_Can_Use/chrome_inspector/chrome_inspector_lab/slides.md
@@ -0,0 +1,5 @@
+# Reported bugs
+
+1. The entire thing doesn't work! The buttons don't work
+2. Something is wrong when I choose scissors
+3. Something is wrong when I choose paper.
